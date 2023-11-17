8085751109b3b2348d25fe97ff1587210b5543d2
Ben Sutter <Ben Sutter>
2023-11-17 01:19:46 -0600

    im still working through tailwind and my header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './styles/generated.css';
+import Header from './Components/header.js';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         <p>
           hi this is my site
         </p>
