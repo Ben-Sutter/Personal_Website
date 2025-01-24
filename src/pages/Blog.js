fc65064e79d87481aefc36f4cff454d19dbb0f77
ben-sutter <42955074+Ben-Sutter@users.noreply.github.com>
2025-01-24 08:36:33 +0000

    added a form

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "../styles/generated.css";
 
-
 const Blog = () => {
   const [blogPosts] = useState([]);
 
@@ -9,13 +8,42 @@ const Blog = () => {
     <div className="flex justify-center">
       <div className="w-full md:w-3/4 p-10">
         <h1 className="text-4xl font-bold mb-4">Blog</h1>
-        {blogPosts.map((post, index) => (
-          <div key={index} className="mb-8">
-            <h2 className="text-2xl font-semibold mb-2">{post.title}</h2>
-            <h4 className="text-lg mb-2">{post.date}</h4>
-            <p className="text-base">{post.content}</p>
+        <form name="contact">
+          <div className="space-y-4">
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                placeholder="Enter your name"
+              />
+            </div>
+
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                placeholder="Enter your email"
+              />
+            </div>
+
+            <div className="text-center">
+              <button
+                type="submit"
+                className="px-2 sm:px-3 py-1 sm:py-2 rounded-lg bg-fuchsia-600 text-fuchsia-200 font-bold hover:bg-fuchsia-700 hover:text-fuchsia-300"
+              >
+                Send
+              </button>
+            </div>
           </div>
-        ))}
+        </form>
       </div>
     </div>
   );
