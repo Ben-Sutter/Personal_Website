1f30bb3bc6428af869d3d0701a1a34d76b0337fd
Ben Sutter <Ben Sutter>
2025-01-25 01:49:09 -0600

    adding styling back

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,47 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from 'react-bootstrap';
+import styled from 'styled-components';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import introText from '../files/general_text/intro.txt';
 import funSectionText from '../files/general_text/funSection.txt';
+import pic from "../files/pictures/framed_buffalo_pic.png";
+
+const StyledContainer = styled(Container)`
+  display: flex;
+  justify-content: center;
+  padding: 2rem;
+`;
+
+const StyledContent = styled.div`
+  width: 100%;
+  padding: 2rem;
+  display: flex;
+  flex-wrap: wrap;
+  @media (min-width: 768px) {
+    flex-wrap: nowrap;
+  }
+`;
+
+const StyledTextSection = styled.div`
+  width: 100%;
+  color: #4b0082; /* Purple color */
+  @media (min-width: 768px) {
+    width: 70%;
+    padding-right: 2rem;
+  }
+`;
+
+const StyledImageSection = styled.div`
+  width: 100%;
+  @media (min-width: 768px) {
+    width: 30%;
+  }
+`;
+
+const StyledImage = styled.img`
+  width: 100%;
+  height: auto;
+`;
 
 const Home = () => {
   const [intro, setIntro] = useState('');
@@ -19,18 +58,21 @@ const Home = () => {
   }, []);
 
   return (
-    <Container className="py-4">
-      <Row className="justify-content-center">
-        <Col md={8}>
-          <h1 className="display-4 font-weight-bold mb-4 text-center">Welcome to My Website!</h1>
-          <p className="lead">{intro}</p>
+    <StyledContainer>
+      <StyledContent>
+        <StyledTextSection>
+          <h1 className="text-4xl font-bold mb-4 text-center">Welcome to My Website!</h1>
+          <p className="mb-8">{intro}</p>
           <div className="rounded">
             <h2 className="text-2xl font-bold mb-2 text-center">Fun Section</h2>
             <p className="mb-2">{funSection}</p>
           </div>
-        </Col>
-      </Row>
-    </Container>
+        </StyledTextSection>
+        <StyledImageSection>
+          <StyledImage src={pic} alt="my pic" />
+        </StyledImageSection>
+      </StyledContent>
+    </StyledContainer>
   );
 };
 
